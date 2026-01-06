ce4de2c82c49166b7bd910cc2da3b8841e656130
Karthik Shekar <karthik@KarthikPC.localdomain>
2026-01-07 09:09:41 +1100

    day02 and day03

---
+++ Day02/Code/Code02.py
@@ -0,0 +1,2 @@
+#Say hello to user
+print("hello, \"friend\"")
---
+++ Day02/Code/code04.py
@@ -0,0 +1,11 @@
+# Ask user for their name
+name = input("What's your name?")
+
+# Remove Whitespaces from str
+name = name.strip()
+
+name = name.capitalize()
+
+# Say hello to user
+print(f"hello, {name}")
+
